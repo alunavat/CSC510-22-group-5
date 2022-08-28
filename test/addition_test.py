a8cacd8aa4025c505b2f35027ce92c1f6eed51b9
Rohit Nair <rohit97n@gmail.com>
2022-08-28 15:41:11 -0400

    Adding new test cases

--- test/addition_test.py
+++ test/addition_test.py
@@ -9,5 +9,17 @@ class Test(unittest.TestCase):
     total = add.Addition(1, 2)
     self.assertEqual(3, total.main())
 
+  def test2(self):
+    total = add.Addition(10, 20)
+    self.assertEqual(30, total.main())
+
+  def test3(self):
+    total = add.Addition(5, 3)
+    self.assertEqual(8, total.main())
+
+  def test1(self):
+    total = add.Addition(6, 18)
+    self.assertEqual(24, total.main())
+
 if __name__ == '__main__':
   unittest.main()
