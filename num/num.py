809b22db81cc2946b5f23cd9864c546bfe852992
Rohit Nair <rohit97n@gmail.com>
2022-09-01 16:11:02 -0400

    Initial Commit

---
+++ num/num.py
@@ -0,0 +1,15 @@
+
+class Num:
+    def __init__(self):
+        pass
+
+    def new(self):
+        pass
+    def add(self):
+        pass
+    def num(self):
+        pass
+    def div(self):
+        pass
+    def mid(self):
+        pass
