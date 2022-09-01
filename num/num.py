bf05a558c01e2629d583cd1f592a7dcadfb94016
Rohit Nair <rohit97n@gmail.com>
2022-09-01 18:20:16 -0400

    Adding nums method

--- num/num.py
+++ num/num.py
@@ -1,29 +1,32 @@
 
-from cmath import inf
-from turtle import Turtle
-
-
 class Num:
     '''
     Num summarizes a stream of numbers.
     '''
+
     def __init__(self):
         self.n = 0
         self.at = 0
         self.name = ""
         self._has = dict()
-        self.lo = inf
-        self.hi = -inf
+        self.lo = float('inf')
+        self.hi = float('-inf')
         self.isSorted = True
         self.w = 1
 
     def new(self):
         pass
+
     def add(self):
         pass
+
     def nums(self):
-        pass
+        if not self.isSorted:
+            self.isSorted = True
+            return sorted(self._has)
+
     def div(self):
         pass
+
     def mid(self):
         pass
