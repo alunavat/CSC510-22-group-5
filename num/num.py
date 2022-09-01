16535a1184c3ad468ccadd1ca455ab73d3c349ee
Pritesh <psurana@ncsu.edu>
2022-09-01 18:00:36 -0400

    Adding add function in Num class

--- num/num.py
+++ num/num.py
@@ -5,11 +5,34 @@ class Num:
 
     def new(self):
         pass
-    def add(self):
-        pass
+
     def nums(self):
         pass
+
+    def add(self, v, pos):
+        # if v~="?" then
+        #     self.n = self.n + 1
+        #     self.lo = math.min(v, self.lo)
+        #     self.hi = math.max(v, self.hi)
+        if v!="?":
+            self.n=n+1
+            self.lo = min(v, self.lo)
+            self.hi = max(v, self.hi)
+            if len(self._has) < the[nums]:
+                pos = 1+ len(self._has)
+            elif random.uniform(0, 1) < the[nums]/self.n:
+                pos = random.randint(1, len(self._has))
+            if pos:
+                self.isSorted = False
+                try:
+                    self._has[pos] = int(v)
+                except:
+                    self._has[pos] = None
+        
+        pass
+
     def div(self):
         pass
+
     def mid(self):
         pass
