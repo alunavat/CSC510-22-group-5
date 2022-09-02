d983a47751908d07162b183f3be25ac2b8bfdd96
Rohit Nair <rohit97n@gmail.com>
2022-09-02 11:21:42 -0400

    Adding sample test for eg.num

--- test/csv_lua_test.py
+++ test/csv_lua_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from csv_lua import sym_code as sym
+from csv_lua import num_code as num
 from csv_lua import settings as settings
 
 class csv_lua_test(unittest.TestCase):
@@ -24,6 +25,17 @@ class csv_lua_test(unittest.TestCase):
         for x in setting_dict:
             print (x + ": " + str(setting_dict[x]))
         self.assertEqual(1, 1)
+
+    def test3(self):
+        num_obj = num.num()
+        for i in range (1,100):
+            num_obj.add(i)
+        mid = num_obj.mid()
+        div = num_obj.div()
+        self.assertTrue(50 <= mid <= 52)
+        self.assertTrue(30.5 <= mid <= 32)
+
+    # def test4(self):
 
     if __name__ == '__main__':
         unittest.main()
